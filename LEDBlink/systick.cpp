83e27efeac6981133685c543d210b7719a705ad3
Keita <koron0902@gmail.com>
2017-12-17 16:49:13 +0900

    Delayのアルゴリズムの修正
    *DelayUsで10[us]未満を指定時の動作を修正
    *Delay用のtickカウンタをDelayスタート時に毎回クリアするように

--- LEDBlink/systick.cpp
+++ LEDBlink/systick.cpp
@@ -12,37 +12,43 @@
 namespace Driver{
 	namespace Tick{
 		volatile static uint64_t tick;
-		static constexpr uint64_t freq = 100000; // T = 10[us]. 過負荷かもしれないからもしかしたら分解能下げる
-		static uint64_t unUsedValue;
+		static constexpr uint64_t freq = 100000; // T = 10[us].
+		volatile static uint32_t unUsedValue;
+		volatile uint64_t upTime;
 
 		void Init(){
 			SysTick_Config(SystemCoreClock / freq);
 			tick = 0;
+			upTime = 0;
 		}
 
-		static inline auto CurrentTick(){
+		static inline void ClearTick(){
+			tick = 0;
+		}
+
+		static inline uint64_t CurrentTick(){
 			return tick;
 		}
 
-		static inline auto CurrentTickUs(){
+		static inline uint64_t CurrentTickUs(){
 			return CurrentTick();
 		}
 
-		static inline auto CurrentTickMs(){
+		static inline uint64_t CurrentTickMs(){
 			return CurrentTick() / 100;
 		}
 
-		void __DelayUs(const uint64_t _us){
-			uint64_t time = CurrentTickUs() + _us / 10;
-			while(CurrentTickUs() < time){
+		void DelayUs(const uint64_t _us){
+			ClearTick();
+			while(CurrentTickUs() <= (_us / 10)){
 				// __NOP()だとCPUが止まるから
 				unUsedValue++;
 			}
 		}
 
 		void DelayMs(const uint64_t _ms){
-			uint64_t time = CurrentTickMs() + _ms;
-			while(CurrentTickMs() < time) {
+			ClearTick();
+			while(CurrentTickMs() < _ms) {
 				unUsedValue++;
 			}
 		}
@@ -55,8 +61,25 @@ namespace Driver{
 			}
 		}
 
+		inline uint64_t GetSystemTimeUs(){
+			return upTime * 10;
+		}
+
+		inline uint64_t GetSystemTimeMs(){
+			return GetSystemTimeUs() / 1000;
+		}
+
+		inline uint64_t GetSystemTimeSec(){
+			return GetSystemTimeMs() / 1000;
+		}
+
+		inline uint64_t GetSystemTimeMin(){
+			return GetSystemTimeSec() / 1000;
+		}
+
 		extern "C" void SysTick_Handler(){
 			tick++;
+			upTime++;
 		}
 	}
 }
--- LEDBlink/systick.hpp
+++ LEDBlink/systick.hpp
@@ -12,10 +12,44 @@
 
 namespace Driver{
 	namespace Tick{
+		/// @brief  Initialize Systick Timer
+		/// @return none
 		void Init();
-		void DelayUS(const uint64_t);
-		void DelayMs(const uint64_t);
-		void Delay(const uint64_t);
+
+		/// @brief     Delay for [us]
+		/// @param _us Sleep time in [us]
+		/// @return    none
+		void DelayUs(const uint64_t _us);
+
+		/// @brief     Delay for [ms]
+		/// @param _ms Sleep time in [ms]
+		/// @return    none
+		void DelayMs(const uint64_t _ms);
+
+		/// @brief      Delay for [s]
+		/// @param _sec Sleep time in [sec]
+		/// @return     none
+		void Delay(const uint64_t _sec);
+
+		/// @brief      Get System Time from Boot in [us]
+		/// @param      none
+		/// @return     System Time in [us]
+		inline uint64_t GetSystemTimeUs();
+
+		/// @brief      Get System Time from Boot in [ms]
+		/// @param      none
+		/// @return     System Time in [ms]
+		inline uint64_t GetSystemTimeMs();
+
+		/// @brief      Get System Time from Boot in [sec]
+		/// @param      none
+		/// @return     System Time in [sec]
+		inline uint64_t GetSystemTimeSec();
+
+		/// @brief      Get System Time from Boot in [min]
+		/// @param      none
+		/// @return     System Time in [min]
+		inline uint64_t GetSystemTimeMin();
 	}
 }
 
